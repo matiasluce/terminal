707f1bdf9307ffba6587579ba7383f621357f76a
Matias Lucero <matiaslucero1717@gmail.com>
2022-06-19 03:17:26 -0300

    mejoras y un poco de css

--- js/main.js
+++ js/main.js
@@ -4,30 +4,27 @@ let prefix = 'matlu@user ~ $ '
 let mtab = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 
 let help = [
-    '<br>',
-    `help    ${mtab}show commands`,
-    'about  information about me',
-    'projects    some projects that I made with much love',
-    '<br>'
+    'Available commands:',
+    'help, about, projects, clear'
 ]
 
 let about = [
-  '<br>',
   'My name is Matías Lucero, I\'m a full-stack developer from Argentina.',
-  'Visit my Github profile to see my projects (https://github.com/matiasluce/).',
-  '<br>'
+  'Visit my Github profile to see my projects (https://github.com/matiasluce/).'
 ]
 
 let projects = [
-  '<br>',
   'Projects:',
   'Monster Energy Website: https://github.com/matiasluce/monster-web/',
-  'Studio Ghibli Films: https://github.com/matiasluce/ghibli-api',
-  '<br>'
+  'Studio Ghibli Films: https://github.com/matiasluce/ghibli-api'
 ]
 
 function resetInput(){
   input.value = '';
+}
+
+function clearScreen(){
+  commands.innerHTML = "";
 }
 
 input.addEventListener('keypress', function (e) {
@@ -47,12 +44,16 @@ input.addEventListener('keypress', function (e) {
           for(c of projects){
             s+= "<p>" + c + "</p>";
           } break;
+        case 'clear':
+          clearScreen(); break;
         default: s+="<p>Comando incorrecto, digite help para conocer los comandos disponibles.</p>";
       }
 
-      resetInput()
-      s+="<br></div>"
-      commands.innerHTML += s;
+      s+="<br></div>";
+      if(input.value != 'clear'){
+        commands.innerHTML += s;
+      }
+      resetInput();
       
     }
 });
